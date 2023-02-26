bf9b1358ff7e003671f42452b42cc7dcd8b00ade
Ajay Sharma <ajaysh73@gmail.com>
2023-02-26 13:42:56 -0500

    Implemented cloudinary repositary

--- Bloggie.Web/Repositories/ImageRepositoryCloudinary.cs
+++ Bloggie.Web/Repositories/ImageRepositoryCloudinary.cs
@@ -1,6 +1,30 @@
-﻿namespace Bloggie.Web.Repositories
+﻿using CloudinaryDotNet;
+
+namespace Bloggie.Web.Repositories
 {
-    public class ImageRepositoryCloudinary
+    public class ImageRepositoryCloudinary : IImageRepository
     {
+        private readonly Account account;
+        public ImageRepositoryCloudinary(IConfiguration configuration)
+        {
+            account = new Account(configuration.GetSection("Cloudinary")["CloudName"],
+                configuration.GetSection("Cloudinary")["ApiKey"],
+                configuration.GetSection("Cloudinary")["ApiSecret"]);
+        }
+        async Task<string> IImageRepository.UploadAsync(IFormFile file)
+        {
+            var client = new Cloudinary(account);
+            var uploadFileResult = await client.UploadAsync(new CloudinaryDotNet.Actions.ImageUploadParams()
+            {
+                File = new FileDescription(file.FileName, file.OpenReadStream()),
+                DisplayName = file.Name
+            });
+
+            if (uploadFileResult != null && uploadFileResult.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                return uploadFileResult.SecureUri.ToString();
+            }
+            return null;
+        }
     }
 }
